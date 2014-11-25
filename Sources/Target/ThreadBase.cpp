a1cdf1877280d26517415d9a215a80d53ef51279
Stephane Sezer <sas@fb.com>
2014-11-24 21:30:39 -0800

    Avoid a double-delete of threads from a process.

--- Sources/Target/ThreadBase.cpp
+++ Sources/Target/ThreadBase.cpp
@@ -19,7 +19,7 @@ ThreadBase::ThreadBase(Process *process, ThreadId tid)
   _process->insert(this);
 }
 
-ThreadBase::~ThreadBase() { _process->remove(this); }
+ThreadBase::~ThreadBase() {}
 
 ErrorCode ThreadBase::prepareSoftwareSingleStep(Address const &) {
   return kErrorUnsupported;
