3e3b39cfa221cd5f7a92941f012e3f218ce16ff2
Stephane Sezer <sas@fb.com>
2015-02-13 14:28:42 -0800

    [WIP] Add PESupport class.

---
+++ Headers/DebugServer2/Support/Windows/PESupport.h
@@ -0,0 +1,24 @@
+//
+// Copyright (c) 2014, Facebook, Inc.
+// All rights reserved.
+//
+// This source code is licensed under the University of Illinois/NCSA Open
+// Source License found in the LICENSE file in the root directory of this
+// source tree. An additional grant of patent rights can be found in the
+// PATENTS file in the same directory.
+//
+
+#ifndef __DebugServer2_Support_Windows_PESupport_h
+#define __DebugServer2_Support_Windows_PESupport_h
+
+#include "DebugServer2/Types.h"
+
+namespace ds2 {
+namespace Support {
+
+class PESupport {
+};
+}
+}
+
+#endif // !__DebugServer2_Support_Windows_PESupport_h
