5f83b40d25ce2c93556dc1d54858a38252494d16
Stephane Sezer <sas@fb.com>
2015-03-02 17:43:18 -0800

    Add builds for Android-X86 to travis.

--- Support/Scripts/prepare-android-toolchain.sh
+++ Support/Scripts/prepare-android-toolchain.sh
@@ -11,7 +11,7 @@
 
 source "$(dirname "$0")/common.sh"
 
-[ $# -eq 1 ] || die "usage: $0 [ arm | x86 | x86_64 ]"
+[ $# -eq 1 ] || die "usage: $0 [ arm | x86 ]"
 
 case "$(uname)" in
   "Linux")  host="linux-x86";;
@@ -20,25 +20,10 @@ case "$(uname)" in
 esac
 
 case "$1" in
-  "arm")
-      toolchain_arch="arm" toolchain_subarch="armeabi" toolchain_triple="arm-linux-androideabi"
-      ;;
-  "x86")
-      toolchain_arch="x86"; toolchain_subarch="x86"; toolchain_triple="i686-linux-android"
-      gcc_triple="x86_64-linux-android"
-      ;;
-  "x86_64")
-      toolchain_arch="x86_64"; toolchain_subarch="x86_64"; toolchain_triple="x86_64-linux-android"
-      gcc_arch="x86"; libdir="lib64"
-      ;;
-  *)
-      die "Unknown architecture '$1'."
-      ;;
+  "arm")  toolchain_arch="arm";     toolchain_triple="arm-linux-androideabi";;
+  "x86")  toolchain_arch="x86_64";  toolchain_triple="x86_64-linux-android";;
+  *)      die "Unknown architecture '$1'." ;;
 esac
-
-gcc_arch="${gcc_arch:-${toolchain_arch}}"
-gcc_triple="${gcc_triple:-${toolchain_triple}}"
-libdir="${libdir:-lib}"
 
 aosp_platform="android-21"
 toolchain_version="4.8"
@@ -60,15 +45,32 @@ clobber_dir() {
 
 mkdir -p "$(dirname "${toolchain_path}")"
 
-aosp_prebuilt_clone "gcc/${host}/${gcc_arch}/${gcc_triple}-${toolchain_version}" "${toolchain_path}"
+aosp_prebuilt_clone "gcc/${host}/$1/${toolchain_triple}-${toolchain_version}" "${toolchain_path}"
 aosp_prebuilt_clone "ndk" "${aosp_ndk_path}"
 
-# Copy sysroot.
-clobber_dir "${aosp_ndk_path}/9/platforms/android-21/arch-${toolchain_arch}" "${toolchain_path}/sysroot"
+# Copy sysroot. For x86_64, libraries are in `usr/lib64/`; we need to copy 32-bit versions of these libraries to `usr/lib/`.
+clobber_dir "${aosp_ndk_path}/9/platforms/${aosp_platform}/arch-${toolchain_arch}" "${toolchain_path}/sysroot"
+if [ "${toolchain_arch}" = "x86_64" ]; then
+  clobber_dir "${aosp_ndk_path}/9/platforms/${aosp_platform}/arch-x86/usr/lib" "${toolchain_path}/sysroot/usr/lib"
+fi
 
 # Copy C++ stuff
 clobber_dir "${aosp_ndk_path}/9/sources/cxx-stl/gnu-libstdc++/${toolchain_version}/include" "${toolchain_path}/include/c++/${toolchain_version}"
-clobber_dir "${aosp_ndk_path}/9/sources/cxx-stl/gnu-libstdc++/${toolchain_version}/libs/${toolchain_subarch}/libgnustl_static.a" "${toolchain_path}/${toolchain_triple}/${libdir}/libstdc++.a"
-clobber_dir "${aosp_ndk_path}/9/sources/cxx-stl/gnu-libstdc++/${toolchain_version}/libs/${toolchain_subarch}/libgnustl_shared.so" "${toolchain_path}/${toolchain_triple}/${libdir}/libgnustl_shared.so"
-clobber_dir "${aosp_ndk_path}/9/sources/cxx-stl/gnu-libstdc++/${toolchain_version}/libs/${toolchain_subarch}/libsupc++.a" "${toolchain_path}/${toolchain_triple}/${libdir}/libsupc++.a"
-clobber_dir "${aosp_ndk_path}/9/sources/cxx-stl/gnu-libstdc++/${toolchain_version}/libs/${toolchain_subarch}/include/bits" "${toolchain_path}/include/c++/${toolchain_version}/${toolchain_triple}/bits"
+copy_cpp_binaries() {
+  clobber_dir "${aosp_ndk_path}/9/sources/cxx-stl/gnu-libstdc++/${toolchain_version}/libs/$1/libgnustl_static.a" "${toolchain_path}/${toolchain_triple}/$2/libstdc++.a"
+  clobber_dir "${aosp_ndk_path}/9/sources/cxx-stl/gnu-libstdc++/${toolchain_version}/libs/$1/libgnustl_shared.so" "${toolchain_path}/${toolchain_triple}/$2/libgnustl_shared.so"
+  clobber_dir "${aosp_ndk_path}/9/sources/cxx-stl/gnu-libstdc++/${toolchain_version}/libs/$1/libsupc++.a" "${toolchain_path}/${toolchain_triple}/$2/libsupc++.a"
+}
+copy_cpp_bits() {
+  clobber_dir "${aosp_ndk_path}/9/sources/cxx-stl/gnu-libstdc++/${toolchain_version}/libs/$1/include/bits" "${toolchain_path}/include/c++/${toolchain_version}/${toolchain_triple}/$2"
+}
+case "$1" in
+  "arm")
+    copy_cpp_binaries "armeabi" "lib"
+    copy_cpp_bits "armeabi" "bits"
+    ;;
+  "x86")
+    copy_cpp_binaries "x86_64" "lib64"; copy_cpp_binaries "x86" "lib"
+    copy_cpp_bits "x86_64" "bits";      copy_cpp_bits "x86" "32/bits"
+    ;;
+esac
