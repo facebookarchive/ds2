c6cc09dc5217645ccd178f6317b6704b46e684c1
Stephane Sezer <sas@fb.com>
2015-03-02 16:30:36 -0800

    Make Android-X86_64 builds work.

---
+++ Support/CMake/Toolchain-Android-X86_64.cmake
@@ -0,0 +1,17 @@
+##
+## Copyright (c) 2014, Facebook, Inc.
+## All rights reserved.
+##
+## This source code is licensed under the University of Illinois/NCSA Open
+## Source License found in the LICENSE file in the root directory of this
+## source tree. An additional grant of patent rights can be found in the
+## PATENTS file in the same directory.
+##
+
+set(CMAKE_SYSTEM_NAME Linux)
+set(ARCH_NAME X86_64)
+
+set(CMAKE_C_COMPILER /tmp/aosp-toolchain/x86_64-linux-android-4.8/bin/x86_64-linux-android-gcc)
+set(CMAKE_CXX_COMPILER /tmp/aosp-toolchain/x86_64-linux-android-4.8/bin/x86_64-linux-android-g++)
+
+set(STATIC 1)
--- Support/Scripts/prepare-android-toolchain.sh
+++ Support/Scripts/prepare-android-toolchain.sh
@@ -11,7 +11,7 @@
 
 source "$(dirname "$0")/common.sh"
 
-[ $# -eq 1 ] || die "usage: $0 [ arm | x86 ]"
+[ $# -eq 1 ] || die "usage: $0 [ arm | x86 | x86_64 ]"
 
 case "$(uname)" in
   "Linux")  host="linux-x86";;
@@ -20,10 +20,25 @@ case "$(uname)" in
 esac
 
 case "$1" in
-  "arm")  toolchain_arch="arm"; toolchain_subarch="armeabi";  toolchain_triple="arm-linux-androideabi";;
-  "x86")  toolchain_arch="x86"; toolchain_subarch="x86";      toolchain_triple="x86_64-linux-android";;
-  *)      die "Unknown architecture '$1'.";;
+  "arm")
+      toolchain_arch="arm" toolchain_subarch="armeabi" toolchain_triple="arm-linux-androideabi"
+      ;;
+  "x86")
+      toolchain_arch="x86"; toolchain_subarch="x86"; toolchain_triple="i686-linux-android"
+      gcc_triple="x86_64-linux-android"
+      ;;
+  "x86_64")
+      toolchain_arch="x86_64"; toolchain_subarch="x86_64"; toolchain_triple="x86_64-linux-android"
+      gcc_arch="x86"; libdir="lib64"
+      ;;
+  *)
+      die "Unknown architecture '$1'."
+      ;;
 esac
+
+gcc_arch="${gcc_arch:-${toolchain_arch}}"
+gcc_triple="${gcc_triple:-${toolchain_triple}}"
+libdir="${libdir:-lib}"
 
 aosp_platform="android-21"
 toolchain_version="4.8"
@@ -45,7 +60,7 @@ clobber_dir() {
 
 mkdir -p "$(dirname "${toolchain_path}")"
 
-aosp_prebuilt_clone "gcc/${host}/${toolchain_arch}/${toolchain_triple}-${toolchain_version}" "${toolchain_path}"
+aosp_prebuilt_clone "gcc/${host}/${gcc_arch}/${gcc_triple}-${toolchain_version}" "${toolchain_path}"
 aosp_prebuilt_clone "ndk" "${aosp_ndk_path}"
 
 # Copy sysroot.
@@ -53,7 +68,7 @@ clobber_dir "${aosp_ndk_path}/9/platforms/android-21/arch-${toolchain_arch}" "${
 
 # Copy C++ stuff
 clobber_dir "${aosp_ndk_path}/9/sources/cxx-stl/gnu-libstdc++/${toolchain_version}/include" "${toolchain_path}/include/c++/${toolchain_version}"
-clobber_dir "${aosp_ndk_path}/9/sources/cxx-stl/gnu-libstdc++/${toolchain_version}/libs/${toolchain_subarch}/libgnustl_static.a" "${toolchain_path}/${toolchain_triple}/lib/libstdc++.a"
-clobber_dir "${aosp_ndk_path}/9/sources/cxx-stl/gnu-libstdc++/${toolchain_version}/libs/${toolchain_subarch}/libgnustl_shared.so" "${toolchain_path}/${toolchain_triple}/lib/libgnustl_shared.so"
-clobber_dir "${aosp_ndk_path}/9/sources/cxx-stl/gnu-libstdc++/${toolchain_version}/libs/${toolchain_subarch}/libsupc++.a" "${toolchain_path}/${toolchain_triple}/lib/libsupc++.a"
+clobber_dir "${aosp_ndk_path}/9/sources/cxx-stl/gnu-libstdc++/${toolchain_version}/libs/${toolchain_subarch}/libgnustl_static.a" "${toolchain_path}/${toolchain_triple}/${libdir}/libstdc++.a"
+clobber_dir "${aosp_ndk_path}/9/sources/cxx-stl/gnu-libstdc++/${toolchain_version}/libs/${toolchain_subarch}/libgnustl_shared.so" "${toolchain_path}/${toolchain_triple}/${libdir}/libgnustl_shared.so"
+clobber_dir "${aosp_ndk_path}/9/sources/cxx-stl/gnu-libstdc++/${toolchain_version}/libs/${toolchain_subarch}/libsupc++.a" "${toolchain_path}/${toolchain_triple}/${libdir}/libsupc++.a"
 clobber_dir "${aosp_ndk_path}/9/sources/cxx-stl/gnu-libstdc++/${toolchain_version}/libs/${toolchain_subarch}/include/bits" "${toolchain_path}/include/c++/${toolchain_version}/${toolchain_triple}/bits"
