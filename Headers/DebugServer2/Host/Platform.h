80a14b6c0c33f51aed504a16cbee72bbf536decc
Stephane Sezer <sas@fb.com>
2015-06-26 13:52:07 -0700

    Uniformize error messages for unsupported targets.
    Also, this commit removes useless #includes of files that don't exist
    yet.

--- Headers/DebugServer2/Host/Platform.h
+++ Headers/DebugServer2/Host/Platform.h
@@ -17,18 +17,8 @@
 #include "DebugServer2/Host/Windows/Platform.h"
 #elif defined(__linux__)
 #include "DebugServer2/Host/Linux/Platform.h"
-#elif defined(__FreeBSD__)
-#include "DebugServer2/Host/FreeBSD/Platform.h"
-#elif defined(__NetBSD__)
-#include "DebugServer2/Host/NetBSD/Platform.h"
-#elif defined(__OpenBSD__)
-#include "DebugServer2/Host/OpenBSD/Platform.h"
-#elif defined(__QNX__)
-#include "DebugServer2/Host/QNX/Platform.h"
-#elif defined(sun)
-#include "DebugServer2/Host/Solaris/Platform.h"
 #else
-#error "Your platform is not supported."
+#error "Target not supported."
 #endif
 
 namespace ds2 {
@@ -38,18 +28,8 @@ namespace Host {
 using Windows::Platform;
 #elif defined(__linux__)
 using Linux::Platform;
-#elif defined(__FreeBSD__)
-using FreeBSD::Platform;
-#elif defined(__NetBSD__)
-using NetBSD::Platform;
-#elif defined(__OpenBSD__)
-using OpenBSD::Platform;
-#elif defined(__QNX__)
-using QNX::Platform;
-#elif defined(sun)
-using Solaris::Platform;
 #else
-#error "Your platform is not supported."
+#error "Target not supported."
 #endif
 }
 }
