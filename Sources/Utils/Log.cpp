7e4bfa4d459ea9e020946bf3a6c93b09ff8f6a70
Stephane Sezer <sas@fb.com>
2015-11-18 17:51:49 -0800

    Log to logcat on Android.
    We use dlsym here so that the binary produced can be used elsewhere
    without major issues.

--- Sources/Utils/Log.cpp
+++ Sources/Utils/Log.cpp
@@ -19,6 +19,7 @@
 #include "DebugServer2/Host/FreeBSD/ExtraWrappers.h"
 #endif
 
+#include <dlfcn.h>
 #include <sstream>
 
 namespace {
@@ -33,6 +34,39 @@ FILE *sOutputStream = stdout;
 FILE *sOutputStream = stderr;
 #endif
 }
+
+#if defined(__ANDROID__)
+#include <android/log.h>
+
+static void androidLogcat(int level, char const *functag, char const *message) {
+  android_LogPriority androidLevel;
+
+  switch (level) {
+#define CONVERT_LEVEL(LEVEL, ANDROID_LEVEL)                                    \
+  case LEVEL:                                                                  \
+    androidLevel = ANDROID_LEVEL;                                              \
+    break;
+    CONVERT_LEVEL(ds2::kLogLevelPacket, ANDROID_LOG_VERBOSE)
+    CONVERT_LEVEL(ds2::kLogLevelDebug, ANDROID_LOG_DEBUG)
+    CONVERT_LEVEL(ds2::kLogLevelInfo, ANDROID_LOG_INFO)
+    CONVERT_LEVEL(ds2::kLogLevelWarning, ANDROID_LOG_WARN)
+    CONVERT_LEVEL(ds2::kLogLevelError, ANDROID_LOG_ERROR)
+    CONVERT_LEVEL(ds2::kLogLevelFatal, ANDROID_LOG_FATAL)
+#undef CONVERT_LEVEL
+  }
+
+  static void *const androidLogLib = dlopen("liblog.so", RTLD_LAZY);
+  if (androidLogLib == nullptr)
+    return;
+  static auto *const androidLogFunc =
+      reinterpret_cast<decltype(&__android_log_print)>(
+          dlsym(androidLogLib, "__android_log_print"));
+  if (androidLogFunc == nullptr)
+    return;
+
+  androidLogFunc(androidLevel, "ds2", "[%s] %s", functag, message);
+}
+#endif
 
 namespace ds2 {
 
@@ -63,16 +97,22 @@ void vLog(int level, char const *classname, char const *funcname,
     va_end(ap_copy);
   } while (required_bytes >= buffer.size());
 
-  ss << '[' << Host::Platform::GetCurrentProcessId() << ']';
+  std::stringstream functag;
+  if (classname != nullptr)
+    functag << classname << "::";
+  functag << funcname;
 
-  if (classname != nullptr) {
-    ss << '[' << classname << "::" << funcname << ']';
-  } else {
-    ss << '[' << funcname << ']';
-  }
+#if defined(__ANDROID__)
+  // If we're on Android pollute logcat as well.
+  androidLogcat(level, functag.str().c_str(), buffer.data());
+#endif
+
+  ss << '[' << Host::Platform::GetCurrentProcessId() << ']';
+  ss << '[' << functag.str() << ']';
 
   char const *color = nullptr;
   char const *label = nullptr;
+
   switch (level) {
   case kLogLevelFatal:
     color = "\x1b[1;31m";
