a2a5eb2f01417ff9e67e8ade10ad0e4225dc626a
Stephane Sezer <sas@fb.com>
2015-12-09 10:38:44 -0800

    Add Toolchain files for Tizen.
    These are basically the Linux toolchain files but built static.

---
+++ Support/CMake/Toolchain-Tizen-ARM.cmake
@@ -0,0 +1,17 @@
+##
+## Copyright (c) 2014, Facebook, Inc.
+## All rights reserved.
+##
+## This source code is licensed under the University of Illinois/NCSA Open
+## Source License found in the LICENSE file in the root directory of this
+## source tree. An additional grant of patent rights can be found in the
+## PATENTS file in the same directory.
+##
+
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR ARM)
+
+set(CMAKE_C_COMPILER arm-linux-gnueabi-gcc-4.7)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabi-g++-4.7)
+
+set(STATIC 1)
