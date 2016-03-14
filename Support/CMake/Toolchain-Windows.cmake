fc437cf8686d1abe39a5593e5ed17a5e8c4742ff
Walter Erquinigo <wallace@fb.com>
2016-03-14 15:11:00 -0700

    Add dummy Toolchain-Windows.cmake file

---
+++ Support/CMake/Toolchain-Windows.cmake
@@ -0,0 +1,12 @@
+##
+## Copyright (c) 2014-present, Facebook, Inc.
+## All rights reserved.
+##
+## This source code is licensed under the University of Illinois/NCSA Open
+## Source License found in the LICENSE file in the root directory of this
+## source tree. An additional grant of patent rights can be found in the
+## PATENTS file in the same directory.
+##
+
+# This is a dummy file used by the .appveyor CMake config. It's empty because
+# Windows doesn't need any additional settings, unlike WinStore.
