b6f08a31f44263fadcb52093392f94b2dde43af4
Stephane Sezer <sas@fb.com>
2016-03-23 20:11:36 -0700

    Add a ds2::Utils function to calculate basename.

---
+++ Headers/DebugServer2/Utils/Paths.h
@@ -0,0 +1,23 @@
+//
+// Copyright (c) 2014-present, Facebook, Inc.
+// All rights reserved.
+//
+// This source code is licensed under the University of Illinois/NCSA Open
+// Source License found in the LICENSE file in the root directory of this
+// source tree. An additional grant of patent rights can be found in the
+// PATENTS file in the same directory.
+//
+
+#ifndef __DebugServer2_Utils_Paths_h
+#define __DebugServer2_Utils_Paths_h
+
+#include <string>
+
+namespace ds2 {
+namespace Utils {
+
+std::string Basename(std::string const &path);
+}
+}
+
+#endif // !__DebugServer2_Utils_Bits_h
---
+++ Sources/Utils/Paths.cpp
@@ -0,0 +1,35 @@
+//
+// Copyright (c) 2014-present, Facebook, Inc.
+// All rights reserved.
+//
+// This source code is licensed under the University of Illinois/NCSA Open
+// Source License found in the LICENSE file in the root directory of this
+// source tree. An additional grant of patent rights can be found in the
+// PATENTS file in the same directory.
+//
+
+#include "DebugServer2/Utils/Paths.h"
+
+namespace ds2 {
+namespace Utils {
+
+std::string Basename(std::string const &path) {
+#if defined(OS_WIN32)
+  char sep = '\\';
+#else
+  char sep = '/';
+#endif
+
+  auto end = path.find_last_not_of(sep);
+  if (end == std::string::npos)
+    return "/";
+
+  auto begin = path.rfind(sep, end);
+  if (begin == std::string::npos) {
+    return path.substr(0, end + 1);
+  } else {
+    return path.substr(begin + 1, end - begin);
+  }
+}
+}
+}
