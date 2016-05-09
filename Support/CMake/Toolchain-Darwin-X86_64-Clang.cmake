3b95c56585d13d3c2ea89f47c59fde3daf20dbbd
Stephane Sezer <sas@fb.com>
2016-05-09 02:21:55 -0700

    Add CMake toolchain files for Darwin.

---
+++ Support/CMake/Toolchain-Darwin-X86_64-Clang.cmake
@@ -0,0 +1,15 @@
+##
+## Copyright (c) 2014-present, Facebook, Inc.
+## All rights reserved.
+##
+## This source code is licensed under the University of Illinois/NCSA Open
+## Source License found in the LICENSE file in the root directory of this
+## source tree. An additional grant of patent rights can be found in the
+## PATENTS file in the same directory.
+##
+
+set(CMAKE_SYSTEM_NAME Darwin)
+set(CMAKE_SYSTEM_PROCESSOR X86_64)
+
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)
