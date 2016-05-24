3d90190a85f58c066ae058175c3e0d3dfc9a0d24
Francis Ricci <fjricci@fb.com>
2016-05-24 12:32:07 -0700

    Implement debug session exit method

--- Headers/DebugServer2/GDBRemote/DebugSessionImpl.h
+++ Headers/DebugServer2/GDBRemote/DebugSessionImpl.h
@@ -146,6 +146,7 @@ protected:
   ErrorCode onTerminate(Session &session, ProcessThreadId const &ptid,
                         StopInfo &stop) override;
   ErrorCode onDetach(Session &session, ProcessId pid, bool stopped) override;
+  [[noreturn]] void onExitServer(Session &session) override;
 
 protected:
   ErrorCode onInsertBreakpoint(Session &session, BreakpointType type,
