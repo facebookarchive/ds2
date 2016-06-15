113051bd0d5e268c28c59a3c4a94afa4dc1a0600
Stephane Sezer <sas@fb.com>
2016-06-15 12:16:22 -0700

    Update documentation.
    Bump to 056c504f9924c0ee3e14c884860430d594ebf74e.

--- search/all_6.js
+++ search/all_6.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['feature',['Feature',['../structds2_1_1_g_d_b_remote_1_1_feature.html',1,'ds2::GDBRemote']]],
+  ['file',['File',['../classds2_1_1_host_1_1_file.html',1,'ds2::Host']]],
   ['flagdef',['FlagDef',['../structds2_1_1_architecture_1_1_flag_def.html',1,'ds2::Architecture']]],
   ['flagset',['FlagSet',['../structds2_1_1_architecture_1_1_flag_set.html',1,'ds2::Architecture']]]
 ];
