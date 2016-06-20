96d0caaf5016af9b9259e54c8e4ba430a43f7ad6
Stephane Sezer (automated commit) <sas@fb.com>
2016-06-20 23:37:39 +0000

    Update documentation.
    Bump to dd42fce3d26eec7aca51057c84a77352adab8b12.

--- search/all_a.js
+++ search/all_a.js
@@ -2,6 +2,10 @@ var searchData=
 [
   ['mach',['Mach',['../classds2_1_1_host_1_1_darwin_1_1_mach.html',1,'ds2::Host::Darwin']]],
   ['machoprocess',['MachOProcess',['../classds2_1_1_target_1_1_darwin_1_1_mach_o_process.html',1,'ds2::Target::Darwin']]],
+  ['make_5funique_5fenabler',['make_unique_enabler',['../structds2_1_1make__unique__enabler.html',1,'ds2']]],
+  ['make_5funique_5fenabler_3c_20process_20_3e',['make_unique_enabler&lt; Process &gt;',['../structds2_1_1make__unique__enabler.html',1,'ds2']]],
+  ['make_5funique_5fenabler_3c_20socket_20_3e',['make_unique_enabler&lt; Socket &gt;',['../structds2_1_1make__unique__enabler.html',1,'ds2']]],
+  ['make_5funique_5fenabler_5fhelper',['make_unique_enabler_helper',['../structds2_1_1make__unique__enabler_1_1make__unique__enabler__helper.html',1,'ds2::make_unique_enabler']]],
   ['memoryregioninfo',['MemoryRegionInfo',['../structds2_1_1_g_d_b_remote_1_1_memory_region_info.html',1,'ds2::GDBRemote']]],
   ['memoryregioninfo',['MemoryRegionInfo',['../structds2_1_1_memory_region_info.html',1,'ds2']]],
   ['messagequeue',['MessageQueue',['../classds2_1_1_message_queue.html',1,'ds2']]]
