2590a4be9e0efec832d5ef29b3fce0c419af4b61
Stephane Sezer (automated commit) <sas@fb.com>
2016-08-03 22:12:35 +0000

    Update documentation.
    Bump to 73d6bd74aaad388d017080faf9899a17b7b73a20.

--- search/all_a.js
+++ search/all_a.js
@@ -6,7 +6,8 @@ var searchData=
   ['make_5funique_5fenabler_3c_20process_20_3e',['make_unique_enabler&lt; Process &gt;',['../structds2_1_1make__unique__enabler.html',1,'ds2']]],
   ['make_5funique_5fenabler_3c_20socket_20_3e',['make_unique_enabler&lt; Socket &gt;',['../structds2_1_1make__unique__enabler.html',1,'ds2']]],
   ['make_5funique_5fenabler_5fhelper',['make_unique_enabler_helper',['../structds2_1_1make__unique__enabler_1_1make__unique__enabler__helper.html',1,'ds2::make_unique_enabler']]],
-  ['memoryregioninfo',['MemoryRegionInfo',['../structds2_1_1_g_d_b_remote_1_1_memory_region_info.html',1,'ds2::GDBRemote']]],
+  ['mappedfileinfo',['MappedFileInfo',['../structds2_1_1_mapped_file_info.html',1,'ds2']]],
   ['memoryregioninfo',['MemoryRegionInfo',['../structds2_1_1_memory_region_info.html',1,'ds2']]],
+  ['memoryregioninfo',['MemoryRegionInfo',['../structds2_1_1_g_d_b_remote_1_1_memory_region_info.html',1,'ds2::GDBRemote']]],
   ['messagequeue',['MessageQueue',['../classds2_1_1_message_queue.html',1,'ds2']]]
 ];
