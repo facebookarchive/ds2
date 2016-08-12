50a5cbdc6a795f7383c093429619b7c05ce45b12
Francis Ricci <fjricci@fb.com>
2016-08-12 10:10:35 -0700

    Refactor hardware breakpoint manager functions
    Move non-architecture-specific functions into a common file

--- Headers/DebugServer2/Core/HardwareBreakpointManager.h
+++ Headers/DebugServer2/Core/HardwareBreakpointManager.h
@@ -11,25 +11,50 @@
 #ifndef __DebugServer2_HardwareBreakpointManager_h
 #define __DebugServer2_HardwareBreakpointManager_h
 
-#include "DebugServer2/Base.h"
-
-#if defined(ARCH_ARM) || defined(ARCH_ARM64)
-#include "DebugServer2/Architecture/ARM/HardwareBreakpointManager.h"
-#elif defined(ARCH_X86) || defined(ARCH_X86_64)
-#include "DebugServer2/Architecture/X86/HardwareBreakpointManager.h"
-#else
-#error "Architecture not supported."
-#endif
+#include "DebugServer2/Core/BreakpointManager.h"
 
 namespace ds2 {
 
-#if defined(ARCH_ARM) || defined(ARCH_ARM64)
-using Architecture::ARM::HardwareBreakpointManager;
-#elif defined(ARCH_X86) || defined(ARCH_X86_64)
-using Architecture::X86::HardwareBreakpointManager;
-#else
-#error "Architecture not supported."
+class HardwareBreakpointManager : public BreakpointManager {
+public:
+  HardwareBreakpointManager(Target::ProcessBase *process);
+  ~HardwareBreakpointManager() override;
+
+public:
+  ErrorCode add(Address const &address, Type type, size_t size,
+                Mode mode) override;
+  ErrorCode remove(Address const &address) override;
+
+public:
+  int hit(Target::Thread *thread, Site &site) override;
+
+protected:
+  ErrorCode enableLocation(Site const &site) override;
+  virtual ErrorCode enableLocation(Site const &site, int idx,
+                                   Target::Thread *thread);
+  ErrorCode disableLocation(Site const &site) override;
+  virtual ErrorCode disableLocation(int idx, Target::Thread *thread);
+
+public:
+  virtual size_t maxWatchpoints();
+
+protected:
+  ErrorCode isValid(Address const &address, size_t size,
+                    Mode mode) const override;
+
+protected:
+  virtual int getAvailableLocation();
+
+protected:
+  std::vector<uint64_t> _locations;
+
+#if defined(ARCH_X86) || defined(ARCH_X86_64)
+protected:
+  virtual ErrorCode disableDebugCtrlReg(uint32_t &ctrlReg, int idx);
+  virtual ErrorCode enableDebugCtrlReg(uint32_t &ctrlReg, int idx, Mode mode,
+                                       int size);
 #endif
+};
 }
 
 #endif // !__DebugServer2_HardwareBreakpointManager_h
---
+++ Sources/Core/HardwareBreakpointManager.cpp
@@ -0,0 +1,117 @@
+//
+// Copyright (c) 2014-present, Facebook, Inc.
+// All rights reserved.
+//
+// This source code is licensed under the University of Illinois/NCSA Open
+// Source License found in the LICENSE file in the root directory of this
+// source tree. An additional grant of patent rights can be found in the
+// PATENTS file in the same directory.
+//
+
+#include "DebugServer2/Core/HardwareBreakpointManager.h"
+#include "DebugServer2/Target/Process.h"
+#include "DebugServer2/Target/Thread.h"
+
+#include <algorithm>
+
+#define super ds2::BreakpointManager
+
+namespace ds2 {
+
+HardwareBreakpointManager::HardwareBreakpointManager(
+    Target::ProcessBase *process)
+    : super(process), _locations(maxWatchpoints(), 0) {}
+
+HardwareBreakpointManager::~HardwareBreakpointManager() {}
+
+ErrorCode HardwareBreakpointManager::add(Address const &address, Type type,
+                                         size_t size, Mode mode) {
+  if (_sites.size() >= maxWatchpoints()) {
+    return kErrorInvalidArgument;
+  }
+
+  if (mode == kModeRead) {
+    DS2LOG(Warning,
+           "read-only watchpoints are unsupported, setting as read-write");
+    mode = static_cast<Mode>(mode | kModeWrite);
+  }
+
+  return super::add(address, type, size, mode);
+}
+
+ErrorCode HardwareBreakpointManager::remove(Address const &address) {
+  auto loc = std::find(_locations.begin(), _locations.end(), address);
+  if (loc != _locations.end()) {
+    _locations[loc - _locations.begin()] = 0;
+  }
+
+  return super::remove(address);
+}
+
+ErrorCode HardwareBreakpointManager::enableLocation(Site const &site) {
+  int idx;
+
+  auto loc = std::find(_locations.begin(), _locations.end(), site.address);
+  if (loc == _locations.end()) {
+    idx = getAvailableLocation();
+    if (idx < 0) {
+      return kErrorInvalidArgument;
+    }
+  } else {
+    idx = loc - _locations.begin();
+  }
+
+  std::set<Target::Thread *> threads;
+  _process->enumerateThreads([&](Target::Thread *thread) {
+    if (thread->state() == Target::Thread::kStopped) {
+      threads.insert(thread);
+    }
+  });
+
+  for (auto thread : threads) {
+    ErrorCode error = enableLocation(site, idx, thread);
+    if (error != kSuccess) {
+      return error;
+    }
+  }
+
+  _locations[idx] = site.address;
+  return kSuccess;
+}
+
+ErrorCode HardwareBreakpointManager::disableLocation(Site const &site) {
+  auto loc = std::find(_locations.begin(), _locations.end(), site.address);
+  if (loc == _locations.end()) {
+    return kErrorInvalidArgument;
+  }
+  int idx = loc - _locations.begin();
+
+  std::set<Target::Thread *> threads;
+  _process->enumerateThreads([&](Target::Thread *thread) {
+    if (thread->state() == Target::Thread::kStopped) {
+      threads.insert(thread);
+    }
+  });
+
+  for (auto thread : threads) {
+    ErrorCode error = disableLocation(idx, thread);
+    if (error != kSuccess) {
+      return error;
+    }
+  }
+
+  return kSuccess;
+}
+
+int HardwareBreakpointManager::getAvailableLocation() {
+  DS2ASSERT(_locations.size() == maxWatchpoints());
+  if (_sites.size() == maxWatchpoints()) {
+    return -1;
+  }
+
+  auto it = std::find(_locations.begin(), _locations.end(), 0);
+  DS2ASSERT(it != _locations.end());
+
+  return (it - _locations.begin());
+}
+}
