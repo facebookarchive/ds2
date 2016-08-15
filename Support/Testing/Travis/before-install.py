e96ab8d839aacc4fac70dcd90d17792d47337e1e
Stephane Sezer <sas@fb.com>
2016-08-15 13:50:38 -0700

    Make sure before-install.py uses brew only on Darwin hosts

--- Support/Testing/Travis/before-install.py
+++ Support/Testing/Travis/before-install.py
@@ -13,22 +13,22 @@ import os
 import sys
 from subprocess import check_call
 
-repositories = []
-keys = []
-
+host = os.uname()[0]
 target = os.getenv('TARGET')
 
-if 'Darwin' in target:
-   check_call('brew update', shell=True)
-   sys.exit(0)
+if host == 'Darwin':
+    check_call('brew update', shell=True)
+elif host == 'Linux':
+    repositories = []
+    keys = []
 
-if target in [ 'Style', 'Registers', 'Linux-X86', 'Linux-X86_64', 'Tizen-X86' ]:
-    repositories.append('ppa:ubuntu-toolchain-r/test')
+    if target in [ 'Style', 'Registers', 'Linux-X86', 'Linux-X86_64', 'Tizen-X86' ]:
+        repositories.append('ppa:ubuntu-toolchain-r/test')
 
-for r in repositories:
-  check_call('sudo add-apt-repository -y "%s"' % r, shell=True)
+    for r in repositories:
+        check_call('sudo add-apt-repository -y "%s"' % r, shell=True)
 
-for k in keys:
-  check_call('wget -O - "%s" | sudo apt-key add -' % k, shell=True)
+    for k in keys:
+        check_call('wget -O - "%s" | sudo apt-key add -' % k, shell=True)
 
-check_call('sudo apt-get update', shell=True)
+    check_call('sudo apt-get update', shell=True)
