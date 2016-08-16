ceb3f7e3ffc01ff31723f10a0a9e727641f30ef4
Stephane Sezer (automated commit) <sas@fb.com>
2016-08-16 22:35:59 +0000

    Update documentation.
    Bump to c6920e81a7df6b0f8b792f3f08b619f9a44a2920.

--- search/classes_7.js
+++ search/classes_7.js
@@ -1,8 +1,7 @@
 var searchData=
 [
   ['handler',['Handler',['../structds2_1_1_g_d_b_remote_1_1_protocol_interpreter_1_1_handler.html',1,'ds2::GDBRemote::ProtocolInterpreter']]],
-  ['hardwarebreakpointmanager',['HardwareBreakpointManager',['../classds2_1_1_architecture_1_1_x86_1_1_hardware_breakpoint_manager.html',1,'ds2::Architecture::X86']]],
-  ['hardwarebreakpointmanager',['HardwareBreakpointManager',['../classds2_1_1_architecture_1_1_a_r_m_1_1_hardware_breakpoint_manager.html',1,'ds2::Architecture::ARM']]],
-  ['hostinfo',['HostInfo',['../structds2_1_1_g_d_b_remote_1_1_host_info.html',1,'ds2::GDBRemote']]],
-  ['hostinfo',['HostInfo',['../structds2_1_1_host_info.html',1,'ds2']]]
+  ['hardwarebreakpointmanager',['HardwareBreakpointManager',['../classds2_1_1_hardware_breakpoint_manager.html',1,'ds2']]],
+  ['hostinfo',['HostInfo',['../structds2_1_1_host_info.html',1,'ds2']]],
+  ['hostinfo',['HostInfo',['../structds2_1_1_g_d_b_remote_1_1_host_info.html',1,'ds2::GDBRemote']]]
 ];
