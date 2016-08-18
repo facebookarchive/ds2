5b579d0e40af1f07e50f46da519fcfbb0ae086e9
Francis Ricci <fjricci@fb.com>
2016-08-18 14:43:10 -0700

    Add script to set up android emulators

---
+++ Support/Testing/Travis/before-script.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+##
+## Copyright (c) 2014-present, Facebook, Inc.
+## All rights reserved.
+##
+## This source code is licensed under the University of Illinois/NCSA Open
+## Source License found in the LICENSE file in the root directory of this
+## source tree. An additional grant of patent rights can be found in the
+## PATENTS file in the same directory.
+##
+
+set -eu
+
+if [[ "$TARGET" = "Android-ARM" && -n "${LLDB_TESTS-}" ]]; then
+  /tmp/android-sdk-linux/tools/emulator64-arm -avd test -no-skin -no-audio -no-window &
+
+  adb wait-for-device
+fi
