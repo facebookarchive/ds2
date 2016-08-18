c7ab35fae29213c1f75177b353f40d837d088214
Stephane Sezer <sas@fb.com>
2016-08-18 00:33:27 -0700

    Move the -m32 flag to toolchain files
    Setting -m32 in the main CMakeLists.txt mostly works but seems like the
    wrong approach because:
      1. cmake tries to build 64-bit binaries when trying to verify if the
         compiler works (after having read the toolchain file);
      2. requires having 64-bit libraries in the sysroot even when building
         a 32-bit target (because of 1.);
      3. would make no sense in the case of a 32-bit only compiler/sysroot.

--- Support/CMake/Toolchain-Android-X86.cmake
+++ Support/CMake/Toolchain-Android-X86.cmake
@@ -13,6 +13,8 @@ set(CMAKE_SYSTEM_PROCESSOR X86)
 
 set(CMAKE_C_COMPILER /tmp/aosp-toolchain/x86_64-linux-android-4.9/bin/x86_64-linux-android-gcc)
 set(CMAKE_CXX_COMPILER /tmp/aosp-toolchain/x86_64-linux-android-4.9/bin/x86_64-linux-android-g++)
+set(CMAKE_C_COMPILER_ARG1 -m32)
+set(CMAKE_CXX_COMPILER_ARG1 -m32)
 
 set(ANDROID 1)
 set(PIE 1)
