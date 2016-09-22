7fe35f735aa943194f3eba63cf7c01991c1428f0
Stephane Sezer <sas@fb.com>
2016-09-22 13:18:25 -0700

    Make it possible to install/run android emulators on macOS

--- Support/Scripts/install-android-emulator.sh
+++ Support/Scripts/install-android-emulator.sh
@@ -9,17 +9,30 @@
 ## PATENTS file in the same directory.
 ##
 
-set -eu
+source "$(dirname "$0")/common.sh"
 
-android_script="/tmp/android-sdk-linux/tools/android"
+[ $# -eq 0 ] || die "usage: $0"
+
+case "$(uname)" in
+  "Linux")  platform_name="linux"; package_extension="tgz";;
+  "Darwin") platform_name="macosx"; package_extension="zip";;
+  *)        die "This script works only on Linux and macOS.";;
+esac
+
+android_sdk_version="r24.4.1"
+android_script="/tmp/android-sdk-${platform_name}/tools/android"
 
 if [ ! -f "$android_script" ] ; then
-  android_sdk_name="android-sdk_r24.4.1-linux"
-  wget "https://dl.google.com/android/$android_sdk_name.tgz" -P "/tmp/"
-  tar -zxf "/tmp/$android_sdk_name.tgz" -C "/tmp/"
+  android_sdk_package="android-sdk_${android_sdk_version}-${platform_name}.${package_extension}"
+  wget "https://dl.google.com/android/${android_sdk_package}" -P /tmp
+  case "${package_extension}" in
+    "zip") unzip -d /tmp "/tmp/${android_sdk_package}";;
+    "tgz") tar -C /tmp -xvf "/tmp/${android_sdk_package}";;
+    *) die "Unknown archive extension."
+  esac
 
-  echo "y" | $android_script update sdk -u -a --filter android-21
-  echo "y" | $android_script update sdk -u -a --filter sys-img-armeabi-v7a-android-21
+  echo "y" | "$android_script" update sdk -u -a --filter android-21
+  echo "y" | "$android_script" update sdk -u -a --filter sys-img-armeabi-v7a-android-21
 
-  echo "no" | $android_script create avd --force -n test -t android-21 --abi armeabi-v7a
+  echo "no" | "$android_script" create avd --force -n test -t android-21 --abi armeabi-v7a
 fi
