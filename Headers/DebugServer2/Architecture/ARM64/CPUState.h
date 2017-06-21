60ba30af7f7eb6f23d56db58afa7fd2faee54b85
Stephane Sezer <sas@fb.com>
2017-06-20 21:09:37 -0700

    Name aarch64 registers correctly
    Not sure why, but some of these were named r<XX> instead of x<XX>.

--- Headers/DebugServer2/Architecture/ARM64/CPUState.h
+++ Headers/DebugServer2/Architecture/ARM64/CPUState.h
@@ -40,8 +40,8 @@ struct CPUState64 {
   union {
     uint64_t regs[31 + 1 + 1 + 1];
     struct {
-      uint64_t x0, x1, x2, r3, x4, x5, x6, r7, x8, x9, x10, r11, x12, x13, x14,
-          r15, x16, x17, x18, x19, x20, x21, x22, x23, x24, x25, x26, x27, x28,
+      uint64_t x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14,
+          x15, x16, x17, x18, x19, x20, x21, x22, x23, x24, x25, x26, x27, x28,
           fp, lr, sp, pc, cpsr;
     };
   } gp;
