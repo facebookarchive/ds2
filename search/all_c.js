90733e2c8150612ac206ee272628dd280d57e48c
Stephane Sezer (automated commit) <sas@fb.com>
2017-07-06 03:18:58 +0000

    Update documentation.
    Bump to 6286c261fd8d05f05f36dc68b7f166d3a93fde5e.

--- search/all_c.js
+++ search/all_c.js
@@ -4,10 +4,10 @@ var searchData=
   ['packetprocessordelegate',['PacketProcessorDelegate',['../structds2_1_1_g_d_b_remote_1_1_packet_processor_delegate.html',1,'ds2::GDBRemote']]],
   ['platform',['Platform',['../classds2_1_1_host_1_1_platform.html',1,'ds2::Host']]],
   ['platformsessionimplbase',['PlatformSessionImplBase',['../classds2_1_1_g_d_b_remote_1_1_platform_session_impl_base.html',1,'ds2::GDBRemote']]],
+  ['process',['Process',['../classds2_1_1_target_1_1_p_o_s_i_x_1_1_process.html',1,'ds2::Target::POSIX']]],
   ['process',['Process',['../classds2_1_1_target_1_1_linux_1_1_process.html',1,'ds2::Target::Linux']]],
   ['process',['Process',['../classds2_1_1_target_1_1_free_b_s_d_1_1_process.html',1,'ds2::Target::FreeBSD']]],
   ['process',['Process',['../classds2_1_1_target_1_1_darwin_1_1_process.html',1,'ds2::Target::Darwin']]],
-  ['process',['Process',['../classds2_1_1_target_1_1_p_o_s_i_x_1_1_process.html',1,'ds2::Target::POSIX']]],
   ['process',['Process',['../classds2_1_1_target_1_1_windows_1_1_process.html',1,'ds2::Target::Windows']]],
   ['processbase',['ProcessBase',['../classds2_1_1_target_1_1_process_base.html',1,'ds2::Target']]],
   ['processinfo',['ProcessInfo',['../structds2_1_1_process_info.html',1,'ds2']]],
@@ -22,8 +22,8 @@ var searchData=
   ['programresult',['ProgramResult',['../structds2_1_1_g_d_b_remote_1_1_program_result.html',1,'ds2::GDBRemote']]],
   ['protocolhandler',['ProtocolHandler',['../structds2_1_1_g_d_b_remote_1_1_protocol_handler.html',1,'ds2::GDBRemote']]],
   ['protocolinterpreter',['ProtocolInterpreter',['../classds2_1_1_g_d_b_remote_1_1_protocol_interpreter.html',1,'ds2::GDBRemote']]],
-  ['ptrace',['PTrace',['../classds2_1_1_host_1_1_p_o_s_i_x_1_1_p_trace.html',1,'ds2::Host::POSIX']]],
+  ['ptrace',['PTrace',['../classds2_1_1_host_1_1_free_b_s_d_1_1_p_trace.html',1,'ds2::Host::FreeBSD']]],
   ['ptrace',['PTrace',['../classds2_1_1_host_1_1_linux_1_1_p_trace.html',1,'ds2::Host::Linux']]],
-  ['ptrace',['PTrace',['../classds2_1_1_host_1_1_darwin_1_1_p_trace.html',1,'ds2::Host::Darwin']]],
-  ['ptrace',['PTrace',['../classds2_1_1_host_1_1_free_b_s_d_1_1_p_trace.html',1,'ds2::Host::FreeBSD']]]
+  ['ptrace',['PTrace',['../classds2_1_1_host_1_1_p_o_s_i_x_1_1_p_trace.html',1,'ds2::Host::POSIX']]],
+  ['ptrace',['PTrace',['../classds2_1_1_host_1_1_darwin_1_1_p_trace.html',1,'ds2::Host::Darwin']]]
 ];
