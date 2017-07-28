2acda637ba4c1dc40a10262789cf0534c9a39321
Alex Langford <apl@fb.com>
2017-07-28 11:09:09 -0700

    Pull correct llvm packages from llvm's apt repositories

--- Support/Testing/Travis/before-install.py
+++ Support/Testing/Travis/before-install.py
@@ -28,10 +28,10 @@ elif host == 'Linux':
 
     def add_llvm_repo():
         add_toolchain_test_repo()
-        (dist,version,name) = platform.linux_distribution()
+        dist = os.getenv("LINUX_DISTRO")
         keys.append('http://llvm.org/apt/llvm-snapshot.gpg.key')
-        llvm_url = 'http://llvm.org/apt/' + name + '/'
-        repo_name = 'llvm-toolchain-' + name + '-4.0'
+        llvm_url = 'http://llvm.org/apt/' + dist + '/'
+        repo_name = 'llvm-toolchain-' + dist + '-4.0'
         repositories.append('deb ' + llvm_url + ' ' + repo_name + ' main')
 
     if target in [ 'Style', 'Registers' ]:
