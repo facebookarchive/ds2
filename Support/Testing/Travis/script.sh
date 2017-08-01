670b8be003f9f028764f521fad8cf581850da1b5
Alex Langford <apl@fb.com>
2017-07-31 18:03:43 -0700

    Remove perl5 and otp installs on travis

--- Support/Testing/Travis/script.sh
+++ Support/Testing/Travis/script.sh
@@ -14,6 +14,8 @@ source "$top/Support/Scripts/common.sh"
 
 cformat="clang-format-4.0"
 
+"$top/Support/Testing/Travis/travis-hacks.sh"
+
 # If we're at the root of the repository, create a build directory and go
 # there; otherwise, assume that we're already in the build directory the user
 # wants to use.
