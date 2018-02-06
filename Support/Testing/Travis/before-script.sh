4b224e615eca6362a04f88bcbee58d69430d736a
Stephane Sezer <sas@fb.com>
2018-02-06 11:26:13 -0800

    Allow installation of multiple Android emulators
    Instead of naming them all the same, we can put them in separate
    directories so they don't conflict. This makes it possible to install
    all the emulators in the docker image easily.

--- Support/Testing/Travis/before-script.sh
+++ Support/Testing/Travis/before-script.sh
@@ -16,9 +16,9 @@ if [[ "$TARGET" == Android-* && -n "${LLDB_TESTS-}" ]]; then
   host_platform_name=$(get_host_platform_name)
 
   case "${TARGET}" in
-    "Android-ARM") emulator_arch="emulator64-arm";;
-    "Android-ARM64") emulator_arch="emulator";;
-    "Android-X86") emulator_arch="emulator64-x86";;
+    "Android-ARM") emulator_arch="emulator64-arm"; arch="arm";;
+    "Android-ARM64") emulator_arch="emulator"; arch="arm64";;
+    "Android-X86") emulator_arch="emulator64-x86"; arch="x86";;
     *)             die "Unknown target '${TARGET}'.";;
   esac
 
@@ -26,6 +26,6 @@ if [[ "$TARGET" == Android-* && -n "${LLDB_TESTS-}" ]]; then
   emulator="${sdk_dir}/emulator/${emulator_arch}"
   adb="${sdk_dir}/platform-tools/adb"
   qt_lib_path="${sdk_dir}/emulator/lib64/qt/lib/"
-  LD_LIBRARY_PATH="${qt_lib_path}${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}" "$emulator" -avd test -gpu off -no-window &
+  LD_LIBRARY_PATH="${qt_lib_path}${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}" "$emulator" -avd "android-test-${arch}" -gpu off -no-window -no-accel &
   "$adb" wait-for-device
 fi
