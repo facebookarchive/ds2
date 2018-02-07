d7c4b394ae3ab8ae15389d7b871840d535a703a6
Alex Langford <apl@fb.com>
2018-02-07 14:05:54 -0800

    Switch to using gcc 5 from 4.9

--- Support/CMake/Toolchain-Tizen-ARM.cmake
+++ Support/CMake/Toolchain-Tizen-ARM.cmake
@@ -11,8 +11,8 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR ARM)
 
-set(CMAKE_C_COMPILER arm-linux-gnueabi-gcc-4.9)
-set(CMAKE_CXX_COMPILER arm-linux-gnueabi-g++-4.9)
+set(CMAKE_C_COMPILER arm-linux-gnueabi-gcc)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabi-g++)
 
 set(TIZEN 1)
 set(STATIC 1)
